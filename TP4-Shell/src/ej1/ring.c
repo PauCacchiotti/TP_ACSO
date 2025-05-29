dd93a13be003f5c84a6b6e1ea85237f5bd00cced
PauCacchiotti <pcacchiotti@udesa.edu.ar>
2025-05-29 19:38:45 -0300

    entrega tp4

--- TP4-Shell/src/ej1/ring.c
+++ TP4-Shell/src/ej1/ring.c
@@ -4,17 +4,80 @@
 #include <unistd.h>
 #include <sys/wait.h>
 
+int main(int argc, char **argv){	
+	int start, pid, n;
+	int buffer[1]; 
 
-int main(int argc, char **argv)
-{	
-	int start, status, pid, n;
-	int buffer[1];
+	if (argc != 4){
+		printf("Uso: anillo <n> <c> <s> \n");
+		exit(0);
+	}
 
-	if (argc != 4){ printf("Uso: anillo <n> <c> <s> \n"); exit(0);}
-    
-    /* Parsing of arguments */
-  	/* TO COMPLETE */
-    printf("Se crearán %i procesos, se enviará el caracter %i desde proceso %i \n", n, buffer[0], start);
-    
-   	/* You should start programming from here... */
+	n = atoi(argv[1]);
+	buffer[0] = atoi(argv[2]);
+	start = atoi(argv[3]);
+
+	if (start < 0 || start >= n) {
+    	fprintf(stderr, "Error: índice de inicio fuera de rango\n");
+    	exit(1);
+	}
+
+	printf("Se crearán %i procesos, se enviará el caracter %i desde proceso %i \n", n, buffer[0], start);
+
+	int padre_pipe[2];
+	if (pipe(padre_pipe) == -1) {
+		perror("pipe padre");
+		exit(1);
+	}
+
+	int pipes[n][2];
+	for (int i = 0; i < n; i++) {
+		if (pipe(pipes[i]) == -1) {
+			perror("pipe");
+			exit(1);
+		}
+	}
+
+	for (int i = 0; i < n; i++) {
+		pid = fork();
+		if (pid == -1) {
+			perror("fork");
+			exit(1);
+		} else if (pid == 0) {
+			for (int j = 0; j < n; j++) {
+				if (j != i) close(pipes[j][0]); 
+				if (j != (i + 1) % n) close(pipes[j][1]); 
+			}
+			close(padre_pipe[0]); 
+			if (i != (start + n - 1) % n)
+				close(padre_pipe[1]); 
+
+			int val;
+			read(pipes[i][0], &val, sizeof(int));
+			val++;
+			if (i == (start + n - 1) % n) {
+				write(padre_pipe[1], &val, sizeof(int));
+			} else {
+				write(pipes[(i + 1) % n][1], &val, sizeof(int));
+			}
+			exit(0);
+		}
+	}
+
+	for (int i = 0; i < n; i++) {
+		if (i != start) close(pipes[i][1]); 
+		close(pipes[i][0]); 
+	}
+	close(padre_pipe[1]); 
+
+	write(pipes[start][1], &buffer[0], sizeof(int));
+	close(pipes[start][1]);
+
+	read(padre_pipe[0], &buffer[0], sizeof(int));
+	close(padre_pipe[0]);
+
+	for (int i = 0; i < n; i++) wait(NULL);
+
+	printf("El valor final recibido por el padre es: %d\n", buffer[0]);
+	return 0;
 }
--- TP4-Shell/src/ej2/shell.c
+++ TP4-Shell/src/ej2/shell.c
@@ -3,45 +3,96 @@
 #include <unistd.h>
 #include <sys/wait.h>
 #include <string.h>
+#include <ctype.h>
 
 #define MAX_COMMANDS 200
 
-int main() {
+void parse_args(char *cmd, char **argv) {
+    int i = 0;
+    while (*cmd) {
+        while (isspace(*cmd)) cmd++; 
+        if (*cmd == '"') {
+            cmd++; 
+            argv[i++] = cmd;
+            while (*cmd && *cmd != '"') cmd++;
+            if (*cmd) *cmd++ = '\0'; 
+        } else if (*cmd) {
+            argv[i++] = cmd;
+            while (*cmd && !isspace(*cmd)) cmd++;
+            if (*cmd) *cmd++ = '\0';
+        }
+    }
+    argv[i] = NULL;
+}
 
+int main() {
     char command[256];
     char *commands[MAX_COMMANDS];
-    int command_count = 0;
 
-    while (1) 
-    {
+    while (1) {
         printf("Shell> ");
-        
-        /*Reads a line of input from the user from the standard input (stdin) and stores it in the variable command */
-        fgets(command, sizeof(command), stdin);
-        
-        /* Removes the newline character (\n) from the end of the string stored in command, if present. 
-           This is done by replacing the newline character with the null character ('\0').
-           The strcspn() function returns the length of the initial segment of command that consists of 
-           characters not in the string specified in the second argument ("\n" in this case). */
+        fflush(stdout);
+
+        if (fgets(command, sizeof(command), stdin) == NULL) break;
         command[strcspn(command, "\n")] = '\0';
 
-        /* Tokenizes the command string using the pipe character (|) as a delimiter using the strtok() function. 
-           Each resulting token is stored in the commands[] array. 
-           The strtok() function breaks the command string into tokens (substrings) separated by the pipe character |. 
-           In each iteration of the while loop, strtok() returns the next token found in command. 
-           The tokens are stored in the commands[] array, and command_count is incremented to keep track of the number of tokens found. */
+        int command_count = 0;
         char *token = strtok(command, "|");
-        while (token != NULL) 
-        {
+        while (token != NULL) {
             commands[command_count++] = token;
             token = strtok(NULL, "|");
         }
 
-        /* You should start programming from here... */
-        for (int i = 0; i < command_count; i++) 
-        {
-            printf("Command %d: %s\n", i, commands[i]);
-        }    
+        int prev_pipe[2] = {-1, -1};
+
+        for (int i = 0; i < command_count; i++) {
+            int curr_pipe[2];
+            if (i < command_count - 1 && pipe(curr_pipe) == -1) {
+                perror("pipe");
+                exit(1);
+            }
+
+            pid_t pid = fork();
+            if (pid == -1) {
+                perror("fork");
+                exit(1);
+            }
+
+            if (pid == 0) {
+                if (i > 0) {
+                    dup2(prev_pipe[0], STDIN_FILENO);
+                    close(prev_pipe[0]);
+                    close(prev_pipe[1]);
+                }
+
+                if (i < command_count - 1) {
+                    dup2(curr_pipe[1], STDOUT_FILENO);
+                    close(curr_pipe[0]);
+                    close(curr_pipe[1]);
+                }
+
+                char *args[64];
+                parse_args(commands[i], args);
+                execvp(args[0], args);
+                perror("execvp");
+                exit(1);
+            }
+
+            if (i > 0) {
+                close(prev_pipe[0]);
+                close(prev_pipe[1]);
+            }
+
+            if (i < command_count - 1) {
+                prev_pipe[0] = curr_pipe[0];
+                prev_pipe[1] = curr_pipe[1];
+            }
+        }
+
+        for (int i = 0; i < command_count; i++) {
+            wait(NULL);
+        }
     }
+
     return 0;
 }
